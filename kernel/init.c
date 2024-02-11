177f478ff01aaf6489e9fa7468e4f1080b777b28
Joshua Jacobs-Rebhun <joshua.jacobs.rebhun94@gmail.com>
2024-02-11 16:31:48 -0500

    Added initialization code in both kernel and arch directories.

--- kernel/init.c
+++ kernel/init.c
@@ -2,9 +2,59 @@
  * Kernel initialization routines.
  */
 
+#include "task.h"
+#include "kheap.h"
+#include "filesystem.h"
+#include "misc.h"
 
+/*
+ * Defined in arch/mips/userspace.S
+ */
+extern void create_userspace();
+
+
+/*
+ * Defined in global_structs.c
+ */
+extern task_table_t task_table;
+extern uint32_t *current_task_register_base;
+extern uint32_t *kernel_register_base;
+
+
+/*
+ * Linker defined symbols
+ */
+extern void *_kernel_stack;
+extern heap_cb_t kernel_heap_cb;
+extern void *_ramdisk_begin;
 
 void init_kernel()
 {
+    create_userspace();
+
+    task_table_init(&task_table);
+
+    current_task_register_base = &task_table.current_task->regs[0];
+    kernel_register_base = &task_table.kernel_regs[0];
+
+    store_kernel_context();
+    init_heap(_kheap_begin);
     
+    // not configurable right now
+    superblock_t superblock;
+    superblock.inode_table_start = 2;
+    superblock.inode_table_num_blocks = 2;
+
+    memcpy(_ramdisk_begin, &superblock, sizeof(superblock_t));
+
+    init_filesystem();
+
+
+    // register device drivers
+    // register device filesystem
+
+
+    // stores the kernel context, loads
+    // main task context, and
+    go_to_main();
 }
--- kernel/task.c
+++ kernel/task.c
@@ -7,10 +7,31 @@
 
 #include "task.h"
 
+extern void*_user_stack;
+extern void _exit_main();
+
 
 void task_table_init(task_table_t *table)
 {
+    table->next_available_task_number = 0;
+
+    table->tasks[0].task_id = table->next_available_task_number;
+    table->next_available_task_number++;
+
+    table->tasks[0].parent_task_id = TASK_ID_NONE;
+    table->tasks[0].state = CREATED;
+    table->tasks[0].num_children = 0;
     
+    // set up task scheduling linked list with root task
+    table->tasks[0].next_task = &table->tasks[0];
+    table->tasks[0].previous_task = &table->tasks[0];
+
+    table->current_task = table->root;
+
+    // save root task registers
+    table->tasks[0].regs[REGISTER_SP] = _user_stack;
+    table->tasks[0].regs[REGISTER_FP] = _user_stack;
+    table->tasks[0].regs[REGISTER_RA] = (uint32_t) _exit_main; // need to define in userspace
 }
 
 void schedule_next_task(task_table_t *table)
